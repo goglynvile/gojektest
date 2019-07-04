408cc362478f1b5b2d4e22f1345eb8a2d172a69b
touristglynvile <glynvile.satago@touristtube.com>
2019-07-04 08:30:37 +0800

    initial files

---
+++ GojekContacts/GojekContacts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GojekContacts
+//
+//  Created by Glynvile Satago on 04/07/2019.
+//  Copyright © 2019 GoSatago. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
